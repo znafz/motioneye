d520682efaaa3bf5c4fa847341843fe37ea13f36
Zach Nafziger <10745968+znafz@users.noreply.github.com>
2018-12-14 01:50:29 +0000

    adding userid file

--- telegrambot/bot/bot.py
+++ telegrambot/bot/bot.py
@@ -7,16 +7,19 @@ from pprint import pprint
 
 if __name__ == "__main__":
     with open('bot/apiKey', 'r') as keyfile:
-        token = keyfile.readline()
-        bot = telepot.Bot(token)
-        latest = ""
-        while(True):
-            files = []
-            for root, dirnames, filenames in os.walk('/var/lib/motioneye/'):
-                for filename in fnmatch.filter(filenames, '*.mp4'):
-                    files.append(os.path.join(root, filename))
-            temp = max(files, key=os.path.getctime)
-            if(temp != latest):
-                print(temp)
-                latest = temp
-            time.sleep(5)
+        with open('bot/userid', 'r') as useridfile:
+            token = keyfile.readline()
+            user = useridfile.readline()
+            bot = telepot.Bot(token)
+            print(bot.getUpdates())
+            latest = ""
+            while(True):
+                files = []
+                for root, dirnames, filenames in os.walk('/var/lib/motioneye/'):
+                    for filename in fnmatch.filter(filenames, '*.mp4'):
+                        files.append(os.path.join(root, filename))
+                temp = max(files, key=os.path.getctime)
+                if(temp != latest):
+                    print(temp)
+                    latest = temp
+                time.sleep(5)
